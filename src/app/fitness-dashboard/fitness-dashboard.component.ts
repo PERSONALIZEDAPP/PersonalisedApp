7c08484dd477d05a14959f4c4d26eda7a84416e9
Divya Margaret <divya.badminton@gmail.com>
2024-07-06 12:59:19 +0530

    created dashboard

---
+++ src/app/fitness-dashboard/fitness-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fitness-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './fitness-dashboard.component.html',
+  styleUrl: './fitness-dashboard.component.css'
+})
+export class FitnessDashboardComponent {
+
+}
